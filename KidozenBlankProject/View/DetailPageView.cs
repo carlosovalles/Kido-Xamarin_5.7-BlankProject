de4aa29004717e014a442e39eea92684fd3f25e1
carlosovalles <carlos.ovalles@kidozen.com>
2015-02-02 13:52:17 -0500

    Committing DataSource Integration

--- KidozenBlankProject/View/DetailPageView.cs
+++ KidozenBlankProject/View/DetailPageView.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 
  Kidozen Blank Project - Xamarin
 
@@ -16,87 +16,85 @@ namespace KidozenBlankProject
 {
 	public class DetailPageView : ContentPage
 	{
-
-		List<Contacts> contacts = new List<Contacts>
+		List<Contacts> contacts;
+		public DetailPageView (XamarinMenu item)
 		{
-			new Contacts("John Doe","john.doe@email.com","+1 (800) 555 5555"),
-			new Contacts("Peter Pan","peter.pan@email.com","+1 (800) 555 5555"),
-			new Contacts("Diomedez Diaz","diomedez.diaz@email.com","+1 (800) 555 5555"),
-			new Contacts("John Doe","john.doe@email.com","+1 (800) 555 5555"),
-
-		};
-
-		public DetailPageView ()
-		{
-			StackLayout baseLayout = new StackLayout{
-				BackgroundColor = Color.White,
-			};
-
-			AbsoluteLayout singleListLayout =  new AbsoluteLayout{
-
-				VerticalOptions = LayoutOptions.Start,
-			};
-
-			var backButton = new Image
+			if (item.DataSourceName == null) {
+				Xamarin.Forms.Device.BeginInvokeOnMainThread(() => Navigation.PopModalAsync(false));
+			} 
+			else //there is a type of item
 			{
-				HeightRequest = 24,
-				WidthRequest = 24,
-				Source = new FileImageSource { File = "arrowblack.png" },
-			};
-			backButton.GestureRecognizers.Add(new TapGestureRecognizer((view, args) =>
-			{
-				Xamarin.Forms.Device.BeginInvokeOnMainThread(() => Navigation.PopModalAsync(true));
-			}));
-			AbsoluteLayout.SetLayoutBounds(backButton, new Rectangle(12f, 24f, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
-			singleListLayout.Children.Add (backButton);
-
-			AbsoluteLayout cellLayout;
-			cellLayout = new AbsoluteLayout
-			{
-				BackgroundColor = Color.White,
-				VerticalOptions = LayoutOptions.FillAndExpand
-			};
-
-			ViewCell cell;
-			ListView detailListView = new ListView
-			{
-				ItemsSource = contacts,
-				HeightRequest = 390,
-				WidthRequest = 345,
-				ItemTemplate = new DataTemplate(() =>
-					{
-
-						Label nameLabel = new Label();
-						nameLabel.SetBinding(Label.TextProperty, "Name" );
-						nameLabel.TextColor = Color.FromHex("444F87");
-						nameLabel.Font = Font.OfSize("Helvetica-Oblique", 18);
-
-						AbsoluteLayout.SetLayoutBounds(nameLabel, new Rectangle(14f, 6f, AbsoluteLayout.AutoSize,50));
-						cellLayout.Children.Add(nameLabel);
+				var ds = KidoManager.SharedInstance.Kido.DataSource (item.DataSourceName);
+				contacts = ds.Query<List<Contacts>> ().Result;
 
 
-						Label phoneLabel = new Label();
-						phoneLabel.SetBinding(Label.TextProperty, "PhoneNumber" );
-						phoneLabel.TextColor = Color.FromHex("3C3C3C");
-						phoneLabel.Font = Font.OfSize("Helvetica-Bold", 12);
+				StackLayout baseLayout = new StackLayout {
+					BackgroundColor = Color.White,
+				};
 
-						AbsoluteLayout.SetLayoutBounds(phoneLabel, new Rectangle(29f, 34f, AbsoluteLayout.AutoSize,50));
-						cellLayout.Children.Add(phoneLabel);
+				AbsoluteLayout singleListLayout = new AbsoluteLayout {
 
-						Label emailLabel = new Label();
-						emailLabel.SetBinding(Label.TextProperty, "Email" );
-						emailLabel.TextColor = Color.FromHex("3C3C3C");
-						emailLabel.Font = Font.OfSize("Helvetica-Bold", 11);
+					VerticalOptions = LayoutOptions.Start,
+				};
 
-						AbsoluteLayout.SetLayoutBounds(emailLabel, new Rectangle(29f, 56f, AbsoluteLayout.AutoSize,50));
-						cellLayout.Children.Add(emailLabel);
+				var backButton = new Image {
+					HeightRequest = 22,
+					WidthRequest = 22,
+					Source = new FileImageSource { File = "arrowblack.png" },
+				};
 
-						cell = new ViewCell()
-						{
-							View = new AbsoluteLayout
-							{
-								Children =
-								{
+				backButton.GestureRecognizers.Add (new TapGestureRecognizer {
+					Command = new Command (() => {
+					Xamarin.Forms.Device.BeginInvokeOnMainThread (() => Navigation.PopModalAsync (true));
+					}),
+				});
+
+				AbsoluteLayout.SetLayoutBounds (backButton, new Rectangle (12f, 24f, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+				singleListLayout.Children.Add (backButton);
+
+
+				AbsoluteLayout cellLayout;
+				cellLayout = new AbsoluteLayout {
+					BackgroundColor = Color.White,
+					VerticalOptions = LayoutOptions.FillAndExpand
+				};
+
+				ViewCell cell;
+				ListView detailListView = new ListView {
+					ItemsSource = contacts,
+					HeightRequest = 390,
+					WidthRequest = 345,
+					ItemTemplate = new DataTemplate (() => {
+
+						Label nameLabel = new Label ();
+						nameLabel.SetBinding (Label.TextProperty, "Name");
+						nameLabel.TextColor = Color.FromHex ("444F87");
+						nameLabel.Font = Font.OfSize ("Helvetica-Oblique", 18);
+
+						AbsoluteLayout.SetLayoutBounds (nameLabel, new Rectangle (14f, 6f, AbsoluteLayout.AutoSize, 50));
+						cellLayout.Children.Add (nameLabel);
+
+
+						Label phoneLabel = new Label ();
+						phoneLabel.SetBinding (Label.TextProperty, "Mobile");
+						phoneLabel.TextColor = Color.FromHex ("3C3C3C");
+						phoneLabel.Font = Font.OfSize ("Helvetica-Bold", 12);
+
+						AbsoluteLayout.SetLayoutBounds (phoneLabel, new Rectangle (29f, 34f, AbsoluteLayout.AutoSize, 50));
+						cellLayout.Children.Add (phoneLabel);
+
+
+						Label emailLabel = new Label ();
+						emailLabel.SetBinding (Label.TextProperty, "Email");
+						emailLabel.TextColor = Color.FromHex ("3C3C3C");
+						emailLabel.Font = Font.OfSize ("Helvetica-Bold", 11);
+
+						AbsoluteLayout.SetLayoutBounds (emailLabel, new Rectangle (29f, 56f, AbsoluteLayout.AutoSize, 50));
+						cellLayout.Children.Add (emailLabel);
+
+						cell = new ViewCell () {
+							View = new AbsoluteLayout {
+								Children = {
 									nameLabel,
 									phoneLabel,
 									emailLabel,
@@ -105,22 +103,21 @@ namespace KidozenBlankProject
 							}
 						};
 
-						cell.View.BackgroundColor = Color.FromHex("FAFAFA");
+						cell.View.BackgroundColor = Color.FromHex ("FAFAFA");
 						// approval.IndexOf
 						return cell;
 
 					})
-			};
+				};
 
-			detailListView.HasUnevenRows = true;
-			detailListView.RowHeight = 84;
-			AbsoluteLayout.SetLayoutBounds(detailListView, new Rectangle(-6f, 98, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+				detailListView.HasUnevenRows = true;
+				detailListView.RowHeight = 84;
+				AbsoluteLayout.SetLayoutBounds (detailListView, new Rectangle (-6f, 98, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
 
-			baseLayout.Children.Add(singleListLayout);
-			baseLayout.Children.Add(detailListView);
-			Content = baseLayout;
-
+				baseLayout.Children.Add (singleListLayout);
+				baseLayout.Children.Add (detailListView);
+				Content = baseLayout;
+			}
 		}
 	}
 }
-
