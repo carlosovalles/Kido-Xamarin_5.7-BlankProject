038715ca60cc8d9977c0b3a52ba13f3500eac9eb
carlosovalles <carlos.ovalles@kidozen.com>
2015-02-09 14:34:45 -0500

    committing Changes.

--- KidozenBlankProject/KidozenServices/KidoSettings.cs
+++ KidozenBlankProject/KidozenServices/KidoSettings.cs
@@ -15,7 +15,7 @@ namespace KidozenBlankProject
 		public static string Marketplace = "yourKidoInstance.com";
 		public static string Application = "applicationName";
 		public static string Key = "ApplicationSecret_SDK_Key";
-		public static string UserName = "User Name";
-		public static string PassWord = "Pass";
+		public static string Username = "User Name";
+		public static string Password = "Pass";
 	}
 }
